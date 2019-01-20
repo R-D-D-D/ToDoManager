8354241fe73006a94c2e13d9743cb08530e4d8b2
Runding <wangrunding@gmail.com>
2019-01-20 15:20:36 +0800

    Remove duplicated directory

--- todomanager/README.md
+++
@@ -1,7 +0,0 @@
-# README
-
-Name: Wang Runding
-Matric no.: A0182884N
-
-Username for the web :runding
-Password: 970610
--- todomanager/app/models/task.rb
+++
@@ -1,4 +0,0 @@
-class Task < ApplicationRecord
-  validates :task, presence: true,
-            length: { minimum: 8 }
-end
